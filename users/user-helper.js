c92dd74088a5792ffa717036e0c6b2752be16d9f
Alec Blakeley <alecblkly@gmail.com>
2020-09-02 17:17:19 -0400

    Created validated user helper -- username, email, and password have minimum lengths.

---
+++ users/user-helper.js
@@ -0,0 +1,17 @@
+function validateUser(user) {
+   let errors = [];
+
+   if (!user.username || user.username.length < 5) {
+      errors.push('Username has to be at least 5 characters in length.');
+   }
+   if (!user.email || user.email.length < 5) {
+      errors.push('Email has to be at least 5 characters in length.');
+   }
+   if (!user.password || user.password.length < 8) {
+      errors.push('Password has to be at least 8 characters in length.');
+   }
+   return {
+      isSuccessful: errors.length > 0 ? false : true,
+      errors,
+   };
+}
